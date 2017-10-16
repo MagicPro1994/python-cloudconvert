2992fca9d59d3792ff8dd0f6912609334e65c6ce
MagicPro1994 <theredlion1994@gmail.com>
2017-10-17 00:06:42 +0700

    Add files via upload

---
+++ listEditing.ts
@@ -0,0 +1,178 @@
+'use strict'
+
+import { commands, window, workspace, ExtensionContext, Position, Range, Selection, TextDocument } from 'vscode';
+import * as vscode from 'vscode';
+let dirtyCounter = 0;
+
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(commands.registerCommand('markdown.extension.onEnterKey', onEnterKey));
+    context.subscriptions.push(commands.registerCommand('markdown.extension.onCtrlEnterKey', () => { onEnterKey('ctrl'); }));
+    context.subscriptions.push(commands.registerCommand('markdown.extension.onTabKey', onTabKey));
+    context.subscriptions.push(commands.registerCommand('markdown.extension.onBackspaceKey', onBackspaceKey));
+    context.subscriptions.push(commands.registerCommand('markdown.extension.checkTaskList', checkTaskList));
+}
+
+function isInFencedCodeBlock(doc: TextDocument, lineNum: number): boolean {
+    let textBefore = doc.getText(new Range(new Position(0, 0), new Position(lineNum, 0)));
+    let matches = textBefore.match(/```.*\r?\n/g);
+    if (matches == null) {
+        return false;
+    } else {
+        return matches.length % 2 != 0;
+    }
+}
+
+async function onEnterKey(modifiers?: string) {
+    let editor = window.activeTextEditor;
+    let cursorPos = editor.selection.active;
+    let line = editor.document.lineAt(cursorPos.line);
+    let textBeforeCursor = line.text.substr(0, cursorPos.character);
+    let textAfterCursor = line.text.substr(cursorPos.character);
+
+    let lineBreakPos = cursorPos;
+    if (modifiers == 'ctrl') {
+        lineBreakPos = line.range.end;
+    }
+
+    if (isInFencedCodeBlock(editor.document, cursorPos.line)) {
+        // Normal behavior
+        if (modifiers == 'ctrl') {
+            return commands.executeCommand('editor.action.insertLineAfter');
+        } else {
+            return commands.executeCommand('type', { source: 'keyboard', text: '\n' });
+        }
+    }
+
+    // If it's an empty list item, remove it
+    if (/^([-+*]|[0-9]+[.)])(| \[[ x]\])$/.test(textBeforeCursor.trim()) && textAfterCursor.trim().length == 0) {
+        return editor.edit(editBuilder => {
+            editBuilder.delete(line.range);
+            editBuilder.insert(line.range.end, '\n');
+        });
+    }
+
+    let matches;
+    if ((matches = /^(\s*[-+*] +(|\[[ x]\] +))[^\[\]]+$/.exec(textBeforeCursor)) !== null) {
+        // Unordered list
+        await editor.edit(editBuilder => {
+            editBuilder.insert(lineBreakPos, `\n${matches[1].replace('[x]', '[ ]')}`);
+        });
+        // Fix cursor position
+        if (modifiers == 'ctrl' && !cursorPos.isEqual(lineBreakPos)) {
+            let newCursorPos = cursorPos.with(line.lineNumber + 1, matches[1].length);
+            editor.selection = new Selection(newCursorPos, newCursorPos);
+        }
+    } else if ((matches = /^(\s*)([0-9]+)([.)])( +)(|\[[ x]\] +)[^\[\]]+$/.exec(textBeforeCursor)) !== null) {
+        // Ordered list
+        let config = workspace.getConfiguration('markdown.extension.orderedList').get<string>('marker');
+        let marker = '1';
+        let leadingSpace = matches[1];
+        let previousMarker = matches[2];
+        let delimiter = matches[3];
+        let trailingSpace = matches[4];
+        let gfmCheckbox = matches[5].replace('[x]', '[ ]');
+        let textIndent = (previousMarker + delimiter + trailingSpace).length;
+        if (config == 'ordered') {
+            marker = String(Number(previousMarker) + 1);
+        }
+        // Add enough trailing spaces so that the text is aligned with the previous list item, but always keep at least one space
+        trailingSpace = " ".repeat(Math.max(1, textIndent - (marker + delimiter).length));
+
+        await editor.edit(editBuilder => {
+            editBuilder.insert(lineBreakPos, `\n${leadingSpace + marker + delimiter + trailingSpace + gfmCheckbox}`);
+        });
+        // Fix cursor position
+        if (modifiers == 'ctrl' && !cursorPos.isEqual(lineBreakPos)) {
+            let newCursorPos = cursorPos.with(line.lineNumber + 1, (leadingSpace + marker + trailingSpace + gfmCheckbox).length);
+            editor.selection = new Selection(newCursorPos, newCursorPos);
+        }
+    } else {
+        // Normal behavior
+        if (modifiers == 'ctrl') {
+            return commands.executeCommand('editor.action.insertLineAfter');
+        } else {
+            return commands.executeCommand('type', { source: 'keyboard', text: '\n' });
+        }
+    }
+}
+
+async function onTabKey() {
+    let editor = window.activeTextEditor;
+    let cursorPos = editor.selection.active;
+    let textBeforeCursor = editor.document.lineAt(cursorPos.line).text.substr(0, cursorPos.character);
+
+    if (isInFencedCodeBlock(editor.document, cursorPos.line)) {
+        // Normal behavior
+        return commands.executeCommand('tab');
+    }
+
+    if (/^\s*([-+*]|[0-9]+[.)]) +$/.test(textBeforeCursor)) {
+        return commands.executeCommand('editor.action.indentLines');
+    } else {
+        // Normal behavior
+        return commands.executeCommand('tab');
+    }
+}
+
+async function onBackspaceKey() {
+    let editor = window.activeTextEditor;
+    let editDocument = editor.document;
+    let cursorPos = editor.selection.active;
+    let textBeforeCursor = editDocument.lineAt(cursorPos.line).text.substr(0, cursorPos.character);
+
+    if (isInFencedCodeBlock(editDocument, cursorPos.line)) {
+        // Normal behavior
+        return commands.executeCommand('deleteLeft');
+    }
+
+    if (/^\s+([-+*]|[0-9]+[.)]) $/.test(textBeforeCursor)) {
+        return commands.executeCommand('editor.action.outdentLines');
+    }
+
+    if (/^([-+*]|[0-9]+[.)]) $/.test(textBeforeCursor)) {
+        return await editor.edit(editBuilder => {
+            editBuilder.delete(new Range(
+                cursorPos.with({ character: 0 }),
+                cursorPos));
+        });
+    }
+    await editor.edit(editBuilder => {
+        editBuilder.delete(new Range(
+            cursorPos.with({ character: 0 }),
+            cursorPos));
+    });
+    let beforeIndex = cursorPos.character - 1;
+    if(beforeIndex > 0) {
+        if(editDocument.isDirty) {
+            dirtyCounter++;
+            console.log(dirtyCounter);
+            commands.executeCommand('cursorMove', {to: 'wrappedLineStart'});
+            commands.executeCommand('cursorMove', {to: 'right', value: beforeIndex + dirtyCounter});
+            commands.executeCommand('deleteLeft');
+            commands.executeCommand('cursorMove', {to: 'wrappedLineStart'});
+            commands.executeCommand('cursorMove', {to: 'right', value: cursorPos.character});
+            return;
+        }
+    }
+    // Normal behavior
+    return commands.executeCommand('deleteLeft');
+}
+
+function checkTaskList() {
+    let editor = window.activeTextEditor;
+    let cursorPos = editor.selection.active;
+    let line = editor.document.lineAt(cursorPos.line).text;
+
+    let matches;
+    if (matches = /^(\s*([-+*]|[0-9]+[.)]) \[) \]/.exec(line)) {
+        return editor.edit(editBuilder => {
+            editBuilder.replace(new Range(cursorPos.with({ character: matches[1].length }), cursorPos.with({ character: matches[1].length + 1 })), 'x');
+        });
+    } else if (matches = /^(\s*([-+*]|[0-9]+[.)]) \[)x\]/.exec(line)) {
+        return editor.edit(editBuilder => {
+            editBuilder.replace(new Range(cursorPos.with({ character: matches[1].length }), cursorPos.with({ character: matches[1].length + 1 })), ' ');
+        });
+    }
+}
+
+export function deactivate() { }
